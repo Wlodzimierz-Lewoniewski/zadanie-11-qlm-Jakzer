de69982fd074f2a69887b5ce72425a1a0760175a
Jakzer <zerebeckijakub5@gmail.com>
2025-01-08 17:31:28 +0100

    i

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import regex as re
+from collections import Counter
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+docs=[]
+n_doc=input()
+for i in range(int(n_doc)):
+    docs.append(input())
+query = input()
+ranking = input()
+ranking=[int(x) for x in ranking if x !=" "]
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+def preprocess_doc(text):
+    text = "".join(re.findall(r"[\w\s]",text))
+    text = re.sub(r"\s",' ',text).strip()
+    text = re.sub(r"\s+",' ',text).lower().split()
+    return text
+docs = [preprocess_doc(doc) for doc in docs]
+query = preprocess_doc(query)
+
+
+def compute_query_likelihood(doc_tokens, query_tokens, lambda_smooth, corpus_tokens):
+    doc_counter = Counter(doc_tokens)
+    corpus_counter = Counter(corpus_tokens)
+    doc_len = len(doc_tokens)
+    corpus_len = len(corpus_tokens)
+
+    likelihood = 1.0
+    for token in query_tokens:
+        pw_d = doc_counter[token] / doc_len if doc_len > 0 else 0
+        pw_c = corpus_counter[token] / corpus_len if corpus_len > 0 else 0
+        smoothed = lambda_smooth * pw_d + (1 - lambda_smooth) * pw_c
+        likelihood *= smoothed
+    return likelihood
+
+
+# Tworzenie korpusu
+total_corpus_tokens = [token for doc in docs for token in doc]
+
+# Obliczanie prawdopodobieństw dla każdego dokumentu
+lambda_smooth = 0.5
+scores = []
+for i, doc_tokens in enumerate(docs):
+    score = compute_query_likelihood(doc_tokens, query, lambda_smooth, total_corpus_tokens)
+    scores.append((i, score))
+
+# Sortowanie wyników na podstawie rankingu i wartości prawdopodobieństwa
+sorted_scores = sorted(scores, key=lambda x: (-x[1], ranking.index(x[0])))
+
+# Wyświetlenie wyników
+wynik = []
+for result in sorted_scores:
+    wynik.append(result[0])
+
+print([int(x) for x in wynik])
